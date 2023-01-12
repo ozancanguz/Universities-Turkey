efd483cb73af1861c686f3fd31166cd4bada5448
Ozan <ozancangu24@gmail.com>
2023-01-12 18:26:22 +0300

    -database functions created in viewmodel

--- app/src/main/java/com/ozancanguz/universities_turkey/viewmodels/ListViewModel.kt
+++ app/src/main/java/com/ozancanguz/universities_turkey/viewmodels/ListViewModel.kt
@@ -2,9 +2,9 @@ package com.ozancanguz.universities_turkey.viewmodels
 
 import android.app.Application
 import android.util.Log
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.*
 import com.ozancanguz.universities_turkey.data.Repository
+import com.ozancanguz.universities_turkey.data.db.UniversityEntity
 import com.ozancanguz.universities_turkey.data.model.University
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
@@ -17,16 +17,40 @@ import javax.inject.Inject
 class ListViewModel@Inject constructor(val repository: Repository,application: Application)
     :AndroidViewModel(application) {
 
+    // ----------RETROFIT
         var universityList=MutableLiveData<University>()
         var job: Job?=null
 
 
+    // ------------ROOM
+    var universityEntityList:LiveData<List<UniversityEntity>> = repository.local.listAllUniversities().asLiveData()
+
+
+    // for room insert university to database
+    fun insertUniversity(universityEntity: UniversityEntity){
+        viewModelScope.launch {
+            repository.local.insertUniversity(universityEntity)
+        }
+    }
+
+    // save to db
+    fun saveUniToDb(university: University){
+        var university=UniversityEntity(university)
+        insertUniversity(university)
+    }
+
+
+
+    // ----------RETROFIT REQUEST
     fun requestApiData(){
         job= CoroutineScope(Dispatchers.IO).launch {
 
             val response=repository.remote.getUniversityList()
             if(response.isSuccessful){
                 universityList.postValue(response.body())
+
+                val universities=universityList.value!!
+                saveUniToDb(universities)
 
             }else{
                 Log.d("listviewmodel","no data"+response.body())
