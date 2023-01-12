221104b03f10344f2b1dd39ea57627412d848a77
Ozan <ozancangu24@gmail.com>
2023-01-12 14:01:17 +0300

    -initial commit

---
+++ app/src/main/java/com/ozancanguz/universities_turkey/ui/login/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.ozancanguz.universities_turkey.ui.login
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ozancanguz.universities_turkey.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+
+    }
+}
