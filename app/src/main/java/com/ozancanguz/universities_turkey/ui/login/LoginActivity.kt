f55a5b1367c7653a4c368b0ac42856cbf2c8f7ef
Ozan <ozancangu24@gmail.com>
2023-01-12 14:35:00 +0300

    -login screen designed

--- app/src/main/java/com/ozancanguz/universities_turkey/ui/login/LoginActivity.kt
+++ app/src/main/java/com/ozancanguz/universities_turkey/ui/login/LoginActivity.kt
@@ -3,12 +3,23 @@ package com.ozancanguz.universities_turkey.ui.login
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.ozancanguz.universities_turkey.R
+import com.ozancanguz.universities_turkey.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+   // private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
 
 
 
