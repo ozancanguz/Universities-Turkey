f2cfff073817b27c2709110a661aa966954b1a3c
Ozan <ozancangu24@gmail.com>
2023-01-12 15:22:50 +0300

    -hilt app class created

---
+++ app/src/main/java/com/ozancanguz/universities_turkey/HiltApplication.kt
@@ -0,0 +1,10 @@
+package com.ozancanguz.universities_turkey
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApplication():Application() {
+
+
+}
