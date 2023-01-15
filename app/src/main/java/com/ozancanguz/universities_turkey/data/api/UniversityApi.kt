7d363dfc11f2faf34d3abccc6efc4eb87f6d0903
Ozan <ozancangu24@gmail.com>
2023-01-16 00:47:56 +0300

    -search fun created in uni api

--- app/src/main/java/com/ozancanguz/universities_turkey/data/api/UniversityApi.kt
+++ app/src/main/java/com/ozancanguz/universities_turkey/data/api/UniversityApi.kt
@@ -3,12 +3,17 @@ package com.ozancanguz.universities_turkey.data.api
 import com.ozancanguz.universities_turkey.data.model.University
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.QueryMap
 
 interface UniversityApi {
 
 
     @GET("search?")
     suspend fun getUniversityList(): Response<University>
+
+    @GET("search?")
+    suspend fun SearchUniversity(  @QueryMap searchQuery: Map<String, String>
+    ): Response<University>
 
 
 
